dc88accecb17dc8799d1eef35e93c3f169c244ce
Bruno Azuara <bruno.azuara@gmail.com>
2019-11-12 10:20:55 +0100

    Subida automática

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: bazuara <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 13:47:31 by bazuara           #+#    #+#             */
-/*   Updated: 2019/11/08 14:31:32 by bazuara          ###   ########.fr       */
+/*   Updated: 2019/11/12 10:19:31 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	char	*s1;
 	char	*s2;
 
+	if (!dst && !src && len > 0)
+		return (0);
 	s1 = (char *)src;
 	s2 = (char *)dst;
 	i = 0;
