6ee11a0d3556eecafbb5599f8cfc23820db1883e
Bruno Azuara <bruno.azuara@gmail.com>
2019-11-18 16:17:21 +0100

    Subida automática

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: bazuara <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 16:54:16 by bazuara           #+#    #+#             */
-/*   Updated: 2019/11/15 17:47:08 by bazuara          ###   ########.fr       */
+/*   Updated: 2019/11/18 16:17:01 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,43 +15,41 @@
 static char	*ft_strnew(int i)
 {
 	char	*str;
+
 	if (!(str = malloc(i * sizeof(char))))
 		return (0);
 	else
 		return (str);
 }
 
-static	int ft_intlen(unsigned int n)
+static	int	ft_intlen(unsigned int n)
 {
 	if (n < 10)
 		return (1);
 	else
-		return (ft_intlen(n/10) + 1);
+		return (ft_intlen(n / 10) + 1);
 }
 
 char		*ft_itoa(int nbr)
 {
-	char *s;
-	int len;
-	int neg;
-	unsigned int n;
+	char			*s;
+	int				len;
+	unsigned int	n;
 
-	neg = 0;
 	if (nbr < 0)
-	{
 		n = -nbr;
-		neg = 1;
-	}
 	else
 		n = nbr;
 	len = ft_intlen(n);
-	if (neg != 0)
+	if (nbr < 0)
 		len++;
 	s = ft_strnew(len + 1);
-	s[len + 1] = '\0';
-	if (neg != 0)
+	if (!s)
+		return (0);
+	s[len] = '\0';
+	if (nbr < 0)
 		s[0] = '-';
-	while (neg < len)
+	while (((nbr < 0) ? 1 : 0) < len)
 	{
 		s[len - 1] = n % 10 + '0';
 		len--;
