0b01cb3a6a689bd1e7bdc9b764170b4791c268e1
Bruno Azuara <bruno.azuara@gmail.com>
2021-03-04 12:34:47 +0100

    formatted ft_printf.h according new norme

--- incs/ft_printf.h
+++ incs/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: bazuara <bazuara@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/06 15:42:46 by bazuara           #+#    #+#             */
-/*   Updated: 2020/11/03 13:38:15 by bazuara          ###   ########.fr       */
+/*   Updated: 2021/03/04 12:34:06 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <limits.h>
 # include "libft.h"
 
-typedef	struct	s_flags
+typedef struct s_flags
 {
 	int			is_minus;
 	int			is_plus;
@@ -37,30 +37,30 @@ typedef	struct	s_flags
 
 const char		*ft_printsymbol(const char *str, t_flags **flags, int **count);
 const char		*ft_checkflags_sym(const char *str, va_list args,
-		t_flags *flags);
+					t_flags *flags);
 const char		*ft_checkflags_num(const char *str, t_flags *flags);
 char			*ft_uitoa(unsigned long long int n);
 char			*ft_strtoupper(char *str);
 const char		*ft_printint(const char *str, va_list args, int **count,
-				t_flags **flags);
+					t_flags **flags);
 int				ft_prespace(t_flags **flags, size_t length, int count,
-				char filler);
+					char filler);
 int				ft_posspace(size_t width, size_t length, int count,
-		char filler);
+					char filler);
 const char		*ft_printstring(const char *str, va_list args, int **count,
-				t_flags **flags);
+					t_flags **flags);
 const char		*ft_printchar(const char *str, va_list args, int **count,
-				t_flags **flags);
+					t_flags **flags);
 int				ft_printword(char *str, t_flags **flags, int count);
 char			*ft_long_itoa(long int nbr);
 char			*ft_uitohex(unsigned int q);
 char			*ft_ultohex(unsigned long q);
 const char		*ft_printuint(const char *str, va_list args, int **count,
-				t_flags **flags);
+					t_flags **flags);
 const char		*ft_printhex(const char *str, va_list args, int **count,
-				t_flags **flags);
+					t_flags **flags);
 const char		*ft_printpointer(const char *str, va_list args, int **count,
-				t_flags **flags);
+					t_flags **flags);
 int				ft_isflag(char *str);
 int				max_int(int a, int b);
 int				ft_isnum(int c);
