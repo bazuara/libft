ec8f58e91ac27ba3bb1e0c0d63b8b4bb283640f7
Bruno Azuara <bruno.azuara@gmail.com>
2021-03-05 11:44:51 +0100

    formatted ft_memchr according to new norme

--- srcs/ft_memchr.c
+++ srcs/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: bazuara <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 16:08:57 by bazuara           #+#    #+#             */
-/*   Updated: 2019/11/08 13:42:44 by bazuara          ###   ########.fr       */
+/*   Updated: 2021/03/05 11:43:58 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t	i;
 	char	*str;
 
-	str = (char*)s;
+	str = (char *)s;
 	i = 0;
 	while (i < n)
 	{
