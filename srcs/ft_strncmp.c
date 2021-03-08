349b22f6110f95d455d8abb55b450e0fea305bf8
Bruno Azuara <bruno.azuara@gmail.com>
2021-03-08 12:59:26 +0100

    format ft_strncmp according new norme

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bazuara <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 12:49:44 by bazuara           #+#    #+#             */
-/*   Updated: 2019/11/12 10:31:36 by bazuara          ###   ########.fr       */
+/*   Updated: 2021/03/08 12:57:33 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n)
