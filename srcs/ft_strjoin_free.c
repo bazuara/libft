079716b28137378e5a69a742b7b9469e597ffbed
Bruno Azuara <bruno.azuara@gmail.com>
2021-03-08 12:52:53 +0100

    format ft_strjoin_free according new norme

--- srcs/ft_strjoin_free.c
+++ srcs/ft_strjoin_free.c
@@ -6,15 +6,15 @@
 /*   By: bazuara <bazuara@student.42madrid.>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/16 09:11:47 by bazuara           #+#    #+#             */
-/*   Updated: 2020/10/16 09:14:04 by bazuara          ###   ########.fr       */
+/*   Updated: 2021/03/08 12:52:18 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/libft.h"
 
-char		*ft_strjoin_free(char *str1, char *str2, int i)
+char	*ft_strjoin_free(char *str1, char *str2, int i)
 {
-	char *temp;
+	char	*temp;
 
 	temp = ft_strjoin(str1, str2);
 	if (i == 1)
