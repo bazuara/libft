e3c5d5c61276cbc7af0a4ad8e4c97a488914ebdc
Bruno Azuara <bruno.azuara@gmail.com>
2021-03-08 12:51:29 +0100

    format ft_strjoin according new norme

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bazuara <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 15:35:53 by bazuara           #+#    #+#             */
-/*   Updated: 2019/11/14 13:01:42 by bazuara          ###   ########.fr       */
+/*   Updated: 2021/03/08 12:50:32 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,14 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*new;
-	size_t	total;
 	size_t	i;
 	size_t	j;
 
 	i = 0;
 	if (!s1 || !s2)
 		return (0);
-	total = ft_strlen(s1) + ft_strlen(s2);
-	if ((new = malloc(((total) * sizeof(char)) + 1)))
+	new = malloc(((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char)) + 1);
+	if (new)
 	{
 		while (i < ft_strlen(s1))
 		{
