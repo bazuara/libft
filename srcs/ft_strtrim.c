3edf874301841eff34b7737ca075a339d499820f
Bruno Azuara <bruno.azuara@gmail.com>
2021-03-08 13:09:56 +0100

    format ft_strtrim according new norme

--- srcs/ft_strtrim.c
+++ srcs/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: bazuara <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 15:56:33 by bazuara           #+#    #+#             */
-/*   Updated: 2019/11/28 12:48:51 by bazuara          ###   ########.fr       */
+/*   Updated: 2021/03/08 13:09:02 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	len = ft_strlen(&s1[i]);
 	if (len != 0)
 		while (s1[i + len - 1]
-				&& ft_strchr((char *)set, s1[i + len - 1]) != NULL)
+			&& ft_strchr((char *)set, s1[i + len - 1]) != NULL)
 			len--;
-	if ((snew = (char*)malloc(sizeof(char) * (len + 1))) == NULL)
+	snew = (char *)malloc(sizeof(char) * (len + 1));
+	if (!snew)
 		return (NULL);
 	ft_memcpy(snew, &s1[i], len);
 	snew[len] = '\0';
